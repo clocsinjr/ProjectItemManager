b09519ec88b7ecc2a76b2ce3dcf979e32c57d412
clocsinjr <clocsinjr@hotmail.com>
2020-05-04 09:16:25 +0200

    updated endpoints and CORS filter

--- src/main/java/com/miso/miniprojectplanner/view/ProjectItemMemberEndpoint.java
+++ src/main/java/com/miso/miniprojectplanner/view/ProjectItemMemberEndpoint.java
@@ -24,6 +24,10 @@ public class ProjectItemMemberEndpoint {
     @PostMapping("/memberChangeStatus")
     public String memberChangeStatus(@RequestParam long piMemId, @RequestParam ProjectItemMember.Status newStatus){
         ProjectItemMember piMem = piService.findProjectItemMemberById(piMemId);
+
+        if (newStatus == ProjectItemMember.Status.ASSIGNED && piMem.getRole() == ""){
+            return "status unchanged: Can't assign member to project without specifying the member's role";
+        }
         piMem.setStatus(newStatus);
         piService.saveProjectItemMember(piMem);
         return "status changed succesfully";
